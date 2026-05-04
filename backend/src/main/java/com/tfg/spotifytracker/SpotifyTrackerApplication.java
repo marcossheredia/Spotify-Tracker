65cda34f864acb7275cfd44c2f2900823e821ed2
marcossheredia <markusklan@gmail.com>
2026-05-04 09:42:12 +0200

    2. Instalacion propia de la carpeta Backend

---
+++ backend/src/main/java/com/tfg/spotifytracker/SpotifyTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.tfg.spotifytracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyTrackerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tfg/spotifytracker/SpotifyTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfg.spotifytracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
